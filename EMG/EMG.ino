30a660c394a6a8903aa04e024c5717451a9f6d6d
DAN-XX <54506454+DAN-XX@users.noreply.github.com>
2019-08-25 21:58:25 +0530

    Add files via upload

---
+++ EMG/EMG.ino
@@ -0,0 +1,29 @@
+#include <Servo.h> 
+Servo myservo;
+const int threshValue = 250;
+const int led=10;
+void setup() 
+{ 
+  myservo.attach(9);
+   pinMode(led, OUTPUT);
+} 
+
+void loop() 
+{ 
+  int value = analogRead(A3);
+
+  if(value < threshValue)        
+  { 
+            myservo.writeMicroseconds(800);  
+            
+  }
+  else 
+  { 
+            myservo.writeMicroseconds(2250);
+           
+  } 
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
+}
